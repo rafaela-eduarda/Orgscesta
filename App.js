a877fd150cc4e12bfc1d0a9ffcb67b1631c9e106
rafaela-eduarda <149601165+rafaela-eduarda@users.noreply.github.com>
2025-03-14 14:22:03 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-web';
+
+export default function App (){
+  return<SafeAreaView>
+  <Text>Olá Mundo</Text>
+  </SafeAreaView>
+}
